7925264f4b8808380d5b9cc839ac64d80c66726a
apoptosis1831 <apoptosis1831@gmail.com>
2022-06-13 23:02:05 +0900

    UPLOAD : COTE-ranking(LV-3)

--- LEVEL3/rank.js
+++ LEVEL3/rank.js
@@ -1,17 +1,35 @@
 function solution(n, results){
-    let tree = [];
-    for(let i=0; i<n; i++){
-        tree.push({
-            parents : [],
-            children : []
-        });
-    }
-    results.forEach((result)=>{
-        const [win, lose] = result;
-        tree[win-1].children.push(lose);
-        tree[lose-1].parents.push(win);
+    let graph = Array.from(Array(n+1), ()=>Array(n+1).fill(false));
+    
+    // 행 인덱스가 열 인덱스 이길 경우 1, 질 경우 -1
+    results.map(([win,lose])=>{
+        graph[win][lose]=1;
+        graph[lose][win]=-1;
+        graph[win][win]=0;
+        graph[lose][lose]=0;
     });
-    console.log(tree)
+
+    // mid가 a를 이기고, b가 mid를 이길 때 b가 a를 이김
+    // 지는 경우도 동일 적용
+    for(let mid = 1; mid<=n; mid++){
+        for(let a=1; a<=n; a++){
+            for(let b=1; b<=n; b++){
+                if(graph[a][mid]===1 && graph[mid][b]===1) graph[a][b]=1;
+                if(graph[a][mid]===-1 && graph[mid][b]===-1) graph[a][b]=-1;
+            }
+        }
+    }
+
+    // false가 행에 포함 안 된 경우 순위가 지정됨
+    let answer=0;
+    for(let i=1; i<=n; i++){
+        let j=1;
+        for(; j<=n; j++){
+            if(graph[i][j]===false) break;
+        }
+        if(j===n+1) answer++;
+    }
+    return answer;
 }
 const n=5;
 const results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]];
---
+++ LEVEL3/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+=>{
+    //     graph[win][lose]=1;
+    //     graph[lose][win]=-1;
+    //     graph[win][win]=0;
+    //     graph[lose][lose]=0;
+    // });
+    // for(let mid = 1; mid<n+1; mid++){
+    //     for(let a=1; a<n+1; a++){
+    //         for(let b=1; b<n+1; b++){
+    //             if(graph[a][mid]===1 && graph[mid][b]===1) graph[a][b]=1;
+    //             if(graph[a][mid]===-1 && graph[mid][b]===-1) graph[a][b]=-1;
+    //         }
+    //     }
+    // }
