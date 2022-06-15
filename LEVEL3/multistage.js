c836db28ba892b14b54a59d3e43e3e5319bc134d
apoptosis1831 <apoptosis1831@gmail.com>
2022-06-16 00:07:39 +0900

    UPDATE : COTE-multistage(add comment)

--- LEVEL3/multistage.js
+++ LEVEL3/multistage.js
@@ -1,13 +1,15 @@
 function solution(enroll, referral, seller, amount){
-    let employee = new Map();
-    let childParent = new Map();
+    let employee = new Map(); // 직원 정보 (key:직원 이름)
+    let childParent = new Map(); // 부모 정보 (key: 자식 이름)
     enroll.forEach((name,idx)=>{
         employee.set(name,0);
         childParent.set(name, referral[idx]);
     });
+
     seller.forEach((name,idx)=>{
         employee.set(name, employee.get(name)+amount[idx]*100);
         let passing = amount[idx]*100;
+        // 넘겨줄 수 있는 돈 1원 미만이거나 부모이름 -이면 반복문 종료
         while(passing/10 > 1){
             employee.set(name, employee.get(name)-Math.floor(passing/10));
             let parent = childParent.get(name);
@@ -16,7 +18,7 @@ function solution(enroll, referral, seller, amount){
             passing = Math.floor(passing/10);
             name = parent;
         }
-    })
+    });
     return Array.from(employee.values());
 }
 // const enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"];
