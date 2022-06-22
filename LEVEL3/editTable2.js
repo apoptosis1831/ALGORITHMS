424172ee7c79a5ccf810a1da8e6b4734660ff007
apoptosis1831 <apoptosis1831@gmail.com>
2022-06-22 19:32:45 +0900

    posting COTE-editTable(LV-3)

--- LEVEL3/editTable2.js
+++ LEVEL3/editTable2.js
@@ -1,17 +1,75 @@
 function solution(n,k,cmd){
-    let n1 = {
-        data : 0
+    const Node = function(index,prev){
+        this.index = index;
+        this.prev= prev;
+        this.next = null
     }
-    let n2 = {
-        data : 1
-    }
-    let n3 = {
-        data : 2
+    
+    let prevNode = new Node(0);
+    let select;
+
+    for(let i=1; i<n; i++){
+        const cntNode = new Node(i, prevNode);
+        prevNode.next=cntNode;
+        prevNode = cntNode;
+        if(i===k){
+            select = cntNode;
+        }
     }
 
-    n1.next = n2;
-    n2.next = n3;
-    console.log(n1)
+    let deleteArr = [];
+
+    const moveSelectNode = (count, direction) =>{
+        for(let i=0; i<count; i++){
+            if(!select[direction]) break;
+            select = select[direction];
+        }
+    }
+
+    const deleteNode = () =>{
+        const prev = select.prev;
+        const next = select.next;
+
+        deleteArr.push(select);
+
+        select = next ? next : prev;
+
+        if(prev) prev.next= next;
+        if(next) next.prev = prev;
+    }
+
+    const recoverNode = () =>{
+        const targetNode = deleteArr.pop();
+
+        const prev = targetNode.prev;
+        const next = targetNode.next;
+
+        if(prev) prev.next = targetNode;
+        if(next) next.prev = targetNode;
+    }
+
+    cmd.forEach((c)=>{
+        switch(c[0]){
+            case 'U':
+                moveSelectNode(c.split(" ")[1], "prev");
+                break;
+            case 'D':
+                moveSelectNode(c.split(" ")[1], "next");
+                break;
+            case "C":
+                deleteNode();
+                break;
+            case "Z":
+                recoverNode();
+                break;
+        }
+    })
+
+    let result = Array(n).fill("O");
+    deleteArr.forEach((node)=>{
+        result[node.index] = "X";
+    });
+    return result.join("");
 }
 const n=8;
 const k=2;
